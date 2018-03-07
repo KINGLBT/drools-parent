42b6ee5587d28ca22aee580d3b1261d9e6289f08
'KINGLBT' <lmluomeng1992@163.com>
2018-03-07 17:03:47 +0800

    提交APP

--- drools-chapter5/src/main/java/org/drools/chapter5/App.java
+++ drools-chapter5/src/main/java/org/drools/chapter5/App.java
@@ -14,6 +14,7 @@ public class App
         // 这是啥玩意啊？
         System.out.println( "Hello World!111" );
         System.out.println( "fdasfdasa" );
+        //fdasfdsa
         System.out.println( "Hello World!111" );
         System.out.println( "Hello World!111" );
         System.out.println( "Hello World!111" );
