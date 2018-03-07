266126ecec28e4c6f21c961b42539249fb1092d4
'KINGLBT' <lmluomeng1992@163.com>
2018-03-07 16:56:04 +0800

    这是我需要体检艾欧的内容哦

--- drools-chapter5/src/main/java/org/drools/chapter5/App.java
+++ drools-chapter5/src/main/java/org/drools/chapter5/App.java
@@ -10,6 +10,8 @@ public class App
     {
         System.out.println( "Hello World!" );
         // 你根本就不懂我啊
+        System.out.println( "Hello World!2222" );
         // 这是啥玩意啊？
+        System.out.println( "Hello World!111" );
     }
 }
